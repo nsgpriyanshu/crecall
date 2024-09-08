92b926f01b7f0f48814fc9d4ad8def2937785267
nsgpriyanshu <166146782+nsgpriyanshu@users.noreply.github.com>
2024-09-08 10:31:27 +0530

    chore: delete outdated files

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Landing from '@/components/Landing'
-
-export default function Home() {
-  return (
-    <main className="min-h-screen items-center justify-center">
-      <Landing />
-    </main>
-  )
-}
